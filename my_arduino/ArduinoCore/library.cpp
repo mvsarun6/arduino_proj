4c9a3007ad075461cf6ec86d66b216f2b81ba6ec
mvsarun6 <mvsarun6@gmail.com>
2020-10-28 22:27:56 +0800

    Initial commit from arduino blink

---
+++ my_arduino/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 28/10/2020 10:26:11 PM
+ * Author : sarun
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
